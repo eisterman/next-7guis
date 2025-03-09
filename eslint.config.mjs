48f13fa7baa113b1e6f6005949aef103ff026299
Federico Pasqua (eisterman) <federico.pasqua.96@gmail.com>
2025-03-09 20:32:57 +0100

    Tentative increase in power for ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,31 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:react/recommended"),
+  {
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the React version
+      },
+    },
+    plugins: {
+      react: eslintPluginReact,
+      reactHooks: eslintPluginReactHooks,
+    }, // Include React and React Hooks plugins
+    rules: {
+      // Add any desired rules or overrides here
+      "react/react-in-jsx-scope": "off", // Next.js doesn't require React in scope
+      "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+      "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "varsIgnorePattern": "^_",
+          "argsIgnorePattern": "^_"
+        }
+      ]
+    },
+  },
 ];
 
 export default eslintConfig;
